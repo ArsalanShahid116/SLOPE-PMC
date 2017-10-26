b6b06154d051b9afd5f0d82307db4552e876b3d3
Arsalan Shahid <arsalan.shahid@ucdconnect.ie>
2017-10-26 22:59:53 +0100

    Updating SLOPE-PMC

--- SLOPE-PMC-PAPI/README.md
+++ SLOPE-PMC-PAPI/README.md
@@ -1,8 +1,24 @@
-SLOPE-PMC-PAPI: Automated PMCs Collection on Intel based multicore servers using PAPI
-=====================================================================================
+# SLOPE-PMC-PAPI: Automated PMCs Collection on Intel based multicore servers using PAPI
+----------------------------------
 
 Copyright 2017 Arsalan Shahid & Ravi Manumachu <br />
 (arsalan.shahid @ ucdconnect.ie, ravi.manumachu @ ucd.ie) <br />
 
 SLOPE-PMC-PAPI is an automated tool for collecting Performance Monitoring Counters <br />
 (PMCs) on Intel based multicore systems using PAPI. <br />
+
+### Required Software 
+----------------------
+1. C+ compiler <br />
+2. cmake <br />
+3. PAPI (Latest version) <br />
+4. numactl <br />
+
+### How to use SLOPE-PMC-PAPI?
+------------------------------
+1. Add benchmarks in 'benchmarks' directory in SLOPE-PMC-PAPI <br />
+2. Go to SLOPE-PMC/SLOPE-PMC-PAPI/src <br />
+3. Specify function to be profiled in 'main.c' <br />
+4. Run 'execute.sh' <br />
+5. Collect results from SLOPE-PMC-PAPI/bin <br />
+6. Example results are given in SLOPE-PMC-PAPI/example-results <br />
--- SLOPE-PMC-PAPI/src/main.c
+++ SLOPE-PMC-PAPI/src/main.c
@@ -1,3 +1,17 @@
+/***************************************************************************
+Copyright 2017 - Arsalan Shahid (arsalan.shahid@ucdconnect.ie)
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or (at
+your option) any later version. This program is distributed in the
+hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE. See the GNU General Public License for more details. You
+should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation,
+Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+***************************************************************************/
+
 #include "main.h"
 
 int counters(
@@ -17,8 +31,7 @@ int counters(
    fprintf(fp, "\nCounter Started: \n");
 
    naiveMxm(fp, "1000");
-   naiveMxv(fp, "20000");
-   //system("../benchmarks/bt.A");   
+   //naiveMxv(fp, "20000"); 
 
    if ( (retval=PAPI_read_counters(values, NUM_EVENTS)) != PAPI_OK)
       ERROR_RETURN(retval);
